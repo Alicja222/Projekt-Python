86923e2c5f2ace106da3501a1eb828cef9643078
vdi-student <vdi-student@EDU-WSB.local>
2022-05-05 21:29:30 +0200

    wrzucenie tekstu do maila

--- programpoczta.py
+++ programpoczta.py
@@ -4,29 +4,23 @@ import win32com.client as win32
 pr = pd.read_excel('Pracownicy.xlsx')
 informacja = []
 #plik = input('Podaj ścieżkę do pliku')
-dane = pr['ID']
+daneid = pr['ID']
+emaile = pr['Email']
 
 i = 0
-for info in dane:
+for info in daneid:
     tekst = (f"Witaj {pr.loc[i, 'Imię']} {pr.loc[i, 'Nazwisko']} twój numer pesel to {pr.loc[i, 'PESEL']}. Twoje wynagrodzenie w miesiącu {pr.loc[i, 'Miesiąc_pensja']} wynosi {pr.loc[i, 'Pensja']} zł, otrzymana premia to {pr.loc[i, 'Premia']} zł, ilość dni przepracowanych to {pr.loc[i, 'Dni_pracy']} dni, pozostało {pr.loc[i, 'Dni_urlopu']} dni urlopu. Pozdrawiamy.")
     informacja.append(tekst)
     print(tekst)
     i += 1
 
-#listamail = []
-#poczta = pr['Email']
-#i = 0
-#for skrzynka in poczta:
- #   konto = poczta
-  #  listamail.append(konto)
-   # i += 1
-
 outlook = win32.Dispatch('outlook.application')
-mail = outlook.CreateItem(0)
 
-mail.To = ";".join(konto)
-mail.Subject = 'Dzień dobry'
-mail.Body = tekst
-mail.CC = 'pw27883@student.wsb.wroclaw.pl'
-
-mail.Send()
+for nr_id, email in zip(daneid, emaile):
+    mail = outlook.CreateItem(0)
+    mail.To = email
+    mail.Subject = 'Dzień dobry'
+    mail.Body = tekst
+    mail.CC = 'pw27883@student.wsb.wroclaw.pl'
+    mail.Send()
+    print('Mail ',daneid, 'wysłany')
